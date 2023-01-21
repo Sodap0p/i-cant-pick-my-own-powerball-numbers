3ca1a51c30d850691d8630c6d0a67718149c715f
Sodap0p <105633828+Sodap0p@users.noreply.github.com>
2023-01-21 10:19:17 -0500

    Update PBGen.c

--- PBGen.c
+++ PBGen.c
@@ -3,6 +3,24 @@
 #include <time.h>
 
 void generate_powerball(int numbers[5], int *powerball) {
+    int i, j;
+
+    srand(time(NULL)); 
+
+    // generate 5 random numbers between 1 and 69
+    for (i = 0; i < 5; i++) {
+        numbers[i] = rand() % 69 + 1;
+        for (j = 0; j < i; j++) {
+            if (numbers[i] == numbers[j]) {
+                i--;
+                break;
+            }
+        }
+    }
+
+    // generate the powerball numb between 1 and 26
+    *powerball = rand() % 26 + 1;
+}
 
 int main() {
     int numbers[5], powerball;
@@ -17,26 +35,4 @@ int main() {
     printf("Powerball: %d\n", powerball);
     
     return 0;
-}
-
-
-
-void generate_powerball(int numbers[5], int *powerball) {
-    int i, j;
-
-    srand(time(NULL)); //This would be the random number generator
-    
-    // generate 5 random numbers between 1 and 69
-    for (i = 0; i < 5; i++) {
-        numbers[i] = rand() % 69 + 1;
-        for (j = 0; j < i; j++) {
-            if (numbers[i] == numbers[j]) {
-                i--;
-                break;
-            }
-        }
-    }
-
-    // generate the powerball numb between 1 and 26
-    *powerball = rand() % 26 + 1;
 }
